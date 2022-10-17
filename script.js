123c891558d31d5bb0a710083d4e485c189145cb
Bruno Luis <Bruno.Dev07@gmail.com>
2022-10-17 13:15:01 -0300

    Final result

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const currencyValueText = document.getElementById('currency-value-text')
 
 const dolar = 5.33
 const euro = 5.19
-
+const bitcoin = 0.0000097
 
 const convertValues = () =>{
     const inputReais = document.getElementById('input-real').value
@@ -15,8 +15,10 @@ const convertValues = () =>{
 
     if(select.value === 'US$ Dólar Americano'){
         currencyValueText.innerHTML = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(inputReais / dolar)
-    } else{
+    } else if(select.value === '€ Euro'){
         currencyValueText.innerHTML = new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}).format(inputReais / euro)
+    } else{
+        currencyValueText.innerHTML = bitcoin * inputReais
     }
 }
 
@@ -29,10 +31,14 @@ changeCurrency = () =>{
         currencyName.innerHTML = 'Euro'
         currencyImg.src = './Assets/europa.svg'
         currencyValueText.innerHTML = '€ 2.000,00'
-    } else{
+    } else if(select.value === 'US$ Dólar Americano'){
         currencyName.innerHTML = 'Dólar Americano'
         currencyImg.src = './Assets/estados-unidos.svg'
         currencyValueText.innerHTML = 'US$ 2.000,00'
+    } else{
+        currencyName.innerHTML = 'Bitcoin'
+        currencyImg.src = './Assets/bitcoin.svg'
+        currencyValueText.innerHTML = '0,097'
     }
 
     convertValues()
